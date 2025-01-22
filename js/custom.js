c3880c8d9cbc85449fb2fb156cc39a676fe7a6d3
Roberto Varas González <149156212+rvarasg@users.noreply.github.com>
2025-01-22 10:14:02 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 });
 
 $(document).ready(function () {
-    // Ensure all popups are hidden when the page loads
+    // 🔹 Ensure all popups are hidden when the page loads
     $(".popup-overlay").hide();
 
     // Open popup when clicking an image container
@@ -39,4 +39,5 @@ $(document).ready(function () {
         }
     });
 });
+
 
